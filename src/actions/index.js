c7611f87934ebe66ccbc0c36e23a6b7ec1d40ea1
Kevin Da Rocha <kev.darocha@gmail.com>
2018-01-27 20:14:04 +0100

    Final commit

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export function selectBook(book) {
+    //Select book is an ActionCreator, it need to return an action, an object with a type property.
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book,
+    };
+}
